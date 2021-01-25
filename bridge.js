e20faf1b09d485827149169ed707f03ce8b76428
cinosion <sugimoto.sion.21010@gmail.com>
2021-01-25 21:10:13 +0900

    update: from cron

--- bridge.js
+++ bridge.js
@@ -1,7 +1,3 @@
 
 
-<<<<<<< HEAD
-var executeTime = "2021/01/25 19:08";
-=======
-var executeTime = "2021/01/25 19:20";
->>>>>>> a00c02e56417ce234631e16b0be14d6c73723180
+var executeTime = "2021/01/25 21:05";
